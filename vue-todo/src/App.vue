6728e0d8a05449d65354072d22ac02ccc2ca8253
Vinicius Baccin <vinicius.baccin@sparkag.com.br>
2023-12-17 18:08:22 -0300

    create project

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
+</style>
